a5b14d706e4a37b57f89342ed1836cf30031c455
gudlana <svitlana.hudym@gmail.com>
2020-11-30 08:57:22 +0200

    second commit

---
+++ StadyGudLana/src/main/java/ua/gudlana/StadyGudLana/model/PostRequestDto.java
@@ -0,0 +1,18 @@
+package ua.gudlana.StadyGudLana.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+public class PostRequestDto {
+
+    public String description;
+
+    public String authorId;
+}
